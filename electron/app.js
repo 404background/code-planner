009eec8be21e3aa3ebefa8ade33496de6e5b5454
404background <404background@gmail.com>
2023-12-27 17:56:43 +0900

    作業保存
    アプリの画面作成
    ・ベースとなる画面構成
    　アイコン、編集欄、出力欄
    ・ノード作成用画面の埋め込み
    
    To do
    ・CSSでのcolumnの高さ設定
    ・ホーム画面の作成
    ・formからPythonに送って実行
    ・STL出力プログラム作成

--- electron/app.js
+++ electron/app.js
@@ -1,14 +1,47 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
+const menuTemplate = require('./menu.js')
 
-const createWindow = () => {
+function createWindow () {
+  const menu = Menu.buildFromTemplate(menuTemplate)
+  Menu.setApplicationMenu(menu) 
+
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    useContentSize: true,
+    titleBarOverlay: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    }
   })
 
-  win.loadFile('app.html')
+  win.loadFile('./app.html')
 }
+
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+    app.quit()
 })

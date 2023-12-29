02ed27f9e131569da18ef608d11c131b5d5270c1
404background <404background@gmail.com>
2023-12-29 16:09:06 +0900

    アプリの画面作成（home、node）
    CSS gridで画面を構成
    pluginフォルダに表示するファイルを入れる形式
    ダークモードは未実装

---
+++ electron/column.js
@@ -0,0 +1,9 @@
+function iconHome() {
+    let workColumn = document.getElementById('work-column')
+    workColumn.innerHTML = "<iframe src=\"./plugin/home/home.html\" id=\"work-iframe\"></iframe>"
+}
+
+function iconNode() {
+    let workColumn = document.getElementById('work-column')
+    workColumn.innerHTML = "<iframe src=\"./plugin/node/node.html\" id=\"work-iframe\"></iframe>"
+}

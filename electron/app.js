a95c6e7f9bb3d5f5ff4f5772c0a68c7cdf78463b
404background <404background@gmail.com>
2023-12-29 16:04:24 +0900

    URLクリック時にブラウザが開くように変更

--- electron/app.js
+++ electron/app.js
@@ -1,20 +1,34 @@
-const { app, BrowserWindow, Menu, ipcMain, nativeTheme } = require('electron/main')
+const { app, BrowserWindow, Menu, screen, shell, ipcMain, nativeTheme } = require('electron/main')
 const path = require('node:path')
 const menuTemplate = require('./menu.js')
 
 function createWindow () {
   const menu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(menu) 
+  const primaryDisplay = screen.getPrimaryDisplay()
+  const { width, height } = primaryDisplay.workAreaSize
 
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: width,
+    height: height,
+    minWidth: 600,
+    minHeight: 400,
     useContentSize: true,
+    title: 'Code Planner',
     titleBarOverlay: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     }
   })
+
+  const handleUrlOpen = (e, url)=>{
+    if( url.match(/^http/)){
+      e.preventDefault()
+      shell.openExternal(url)
+    }
+  }
+  win.webContents.on('will-navigate', handleUrlOpen);
+  win.webContents.on('new-window', handleUrlOpen);
 
   win.loadFile('./app.html')
 }

f7ac97c51e268534cd8b63d15435c01178ac9bc8
404background <404background@gmail.com>
2024-01-04 00:37:30 +0900

    esp-idf-v5.1.2に変更

--- windows/mcu-setup.bat
+++ windows/mcu-setup.bat
@@ -9,13 +9,12 @@ call git clone https://github.com/Moddable-OpenSource/moddable.git
 call cd %MODDABLE%\build\makefiles\win
 call build
 @REM setup ESP-IDF
-call set IDF_PATH=C:\Espressif\frameworks\esp-idf-v5.1.1
+call set IDF_PATH=C:\Espressif\frameworks\esp-idf-v5.1.2
 call set IDF_TOOLS_PATH=C:\Espressif
 call cd %USERPROFILE%
 call mkdir esp32
-call cd %USERPROFILE%\esp32
+call cd esp32
 call git clone -b v5.1.2 --recursive https://github.com/espressif/esp-idf.git
-call cd esp-idf
-call install.bat
 call cd %IDF_PATH%
 call %IDF_TOOLS_PATH%\idf_cmd_init.bat
+call install.bat

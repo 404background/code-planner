9dcf81c2c710b8b1f8fa102e8f58e8d32786edc5
404background <404background@gmail.com>
2024-01-07 00:33:44 +0900

    nodeプラグインに起動ボタンを追加

--- electron/plugin/node/node.js
+++ electron/plugin/node/node.js
@@ -1,11 +1,31 @@
 function iconNode() {
-    let workColumn = document.getElementById('work-column')
-    while(workColumn.firstChild) {
-      workColumn.removeChild(workColumn.firstChild)
-    }
-
-    let webview = document.createElement('webview')
-    webview.setAttribute('id', 'node-red')
-    webview.setAttribute('src', 'http://localhost:8000/red')
-    workColumn.appendChild(webview)
+  let workColumn = document.getElementById('work-column')
+  while(workColumn.firstChild) {
+    workColumn.removeChild(workColumn.firstChild)
   }
+
+  let StartNodeRED = document.createElement('button')
+  StartNodeRED.setAttribute('id', 'start-node-red')
+  StartNodeRED.setAttribute('onclick', 'StartNodeRED()')
+  StartNodeRED.innerHTML = 'Start Node-RED'
+  workColumn.appendChild(StartNodeRED)
+
+  let DisplayReloadNode = document.createElement('button')
+  DisplayReloadNode.setAttribute('id', 'display-reload-node')
+  DisplayReloadNode.setAttribute('onclick', 'DisplayReloadNode()')
+  DisplayReloadNode.innerHTML = 'Display Reload'
+  workColumn.appendChild(DisplayReloadNode)
+
+  let webview = document.createElement('webview')
+  webview.setAttribute('id', 'node-red')
+  webview.setAttribute('src', 'http://localhost:8000/red')
+  workColumn.appendChild(webview)
+}
+
+function StartNodeRED() {
+  window.os.exec('npm run node-red')
+}
+
+function DisplayReloadNode() {
+  iconNode()
+}

8ce78301f0ade048d8ca9283d49ff1c6d1f1cb5d
404background <404background@gmail.com>
2024-03-17 16:01:22 +0900

    Node-RED MCUの環境構築プログラムを修正

--- windows/mcu-hello.bat
+++ windows/mcu-hello.bat
@@ -1,3 +1,4 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\\VC\Auxiliary\Build\vcvars32.bat"
 call cd ..
 call set MODDABLE=C:\pjt\moddable
 call cd %MODDABLE%\examples\helloworld
